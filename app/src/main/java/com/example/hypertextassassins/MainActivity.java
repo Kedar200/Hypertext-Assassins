ef3b31a75af910a3233644c86a254e7ae5723187
HariCharan-91 <hari996326@gmail.com>
2022-11-13 18:27:35 +0530

    Spalsh Screen Updated

--- app/src/main/java/com/example/hypertextassassins/MainActivity.java
+++ app/src/main/java/com/example/hypertextassassins/MainActivity.java
@@ -31,14 +31,11 @@ public class MainActivity extends AppCompatActivity {
     LottieAnimationView assassin;
     TextView logoheader;
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference usersCollectionRef = db.collection("Database");
-        DocumentReference alovelaceDocumentRef = db.document("Database/Users");
         mAuth=FirebaseAuth.getInstance();
         Handler handler = new Handler();
         Intent Dashboard_intent = new Intent(this, dashboard.class);
@@ -51,20 +48,6 @@ public class MainActivity extends AppCompatActivity {
         assassin.animate().setDuration(3000);
         fadee();
 
-        db.collection("User").document("user2")
-                .set(new User("gaja","Suraj","panya"))
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully written!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(Exception e) {
-                        Log.w(TAG, "Error writing document", e);
-                    }
-                });
 
 
 
@@ -76,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                 } else {
                     Log.d("Hello", "Not Logged in");
-                    startActivity(Sign_in_intent);
+                    startActivity(Dashboard_intent);
                     finish();
                 }
 
